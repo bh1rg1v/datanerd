60df85f2fc3cce6d566b5d211961c82c4c7f1158
bh1rg1v <manukondabhargavram@gmail.com>
2026-05-25 16:12:54 +0530

    added frontend configuration to deploy on vercel

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (event) => {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/waitlist",
+            "https://datanerd-czhg.onrender.com/",
             {
                 method: "POST",
 
